6e9f58545f6fd7e240d3c57246567913b2923260
Indra Gunawan <indra.plus.gunawan@gmail.com>
2016-05-11 20:49:20 +0800

    Added /folowing path

--- HelloAngular2/github/Github.js
+++ HelloAngular2/github/Github.js
@@ -22,6 +22,14 @@ app.get('/callback',function(req,res) {
 	return githubOAuth.callback(req, res)
 });
 
+app.get('/following',function(req,res) {
+	github.me.following.read()
+	.then(function(users){
+		res.setHeader('Content-Type', 'application/json');
+		res.send(users);
+	});
+});
+
 app.get('/following/:userid',function(req,res) {
 	github.users(req.params.userid).following.read()
 	.then(function(users){
@@ -40,15 +48,5 @@ githubOAuth.on('token', function(token, serverResponse) {
 	github = new octokat({
 		token: token.access_token
 	});
-
-	github.me.following.read()
-	.then(function(users){
-		var _users = JSON.parse(users)
-		console.log('I am following below user(s):');
-		for (var i = 0;i < _users.length;i++) {
-			console.log(_users[i].login);
-		}
-	});
-
 	serverResponse.sendFile(__dirname + '/static/index.html');
 })
